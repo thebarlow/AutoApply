b8b848f7b0d6b07e359faa01fa01df3eefafdadf
Matthew Barlow <barlowmatt96@gmail.com>
2026-05-28 00:14:24 -0400

    [chore] Add .dev flag to skip first-time setup check

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0"
 
+if exist "%~dp0.dev" goto skip_setup
 if exist "%~dp0setup.bat" (
     echo [start] First-time setup detected. Running setup.bat...
     call "%~dp0setup.bat"
@@ -10,6 +11,7 @@ if exist "%~dp0setup.bat" (
         exit /b 1
     )
 )
+:skip_setup
 
 call .venv\Scripts\activate.bat
 
